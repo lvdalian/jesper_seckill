2b0190709d8e7eb40ad4bb99e8dd6a2739b67769
lvyuhao <35132801+lvdalian@users.noreply.github.com>
2018-11-21 10:29:08 +0800

    1.修改代码,添加注释

--- src/main/java/edu/nyist/seckill/bean/User.java
+++ src/main/java/edu/nyist/seckill/bean/User.java
@@ -5,16 +5,41 @@ import lombok.Data;
 import java.util.Date;
 
 /**
- * Created by jiangyunxiong on 2018/5/22.
+ * @author rogers
+ * @version 1.0
  */
 @Data
 public class User {
+    /**
+     * 用户id
+     */
     private Long id;
+    /**
+     * 昵称
+     */
     private String nickname;
+    /**
+     * MD5(MD5(pass明文+固定salt)+salt
+     */
     private String password;
+    /**
+     * 混淆盐
+     */
     private String salt;
+    /**
+     * 头像，云存储的ID
+     */
     private String head;
+    /**
+     * 注册时间
+     */
     private Date registerDate;
+    /**
+     * 上次登录时间
+     */
     private Date lastLoginDate;
+    /**
+     * 登录次数
+     */
     private Integer loginCount;
 }
